3bbbf3760cf211433fc6cdb9ee5e90e5d1541fb9
Avery Howard <avery.howard.1@us.af.mil>
2022-11-01 10:21:26 -0400

    Linting

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -39,7 +39,10 @@ class ModelTests(TestCase):
 
     def test_create_new_superuser(self):
         """ Test creating a new superuser """
-        user = get_user_model().objects.create_superuser('test@example.com', 'test123')
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
